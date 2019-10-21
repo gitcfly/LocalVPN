da75b5af6c792293a7050c6d2f9e3ecea015ad2e
chenkaiju <1750994840@qq.com>
2019-10-21 21:21:22 +0800

    开始改造成非队列模式
    Change-Id: Ib4296550a95c4bcbe11cc7adb054980c8185f193

--- app/src/main/java/xyz/hexene/localvpn/ProxyServer.java
+++ app/src/main/java/xyz/hexene/localvpn/ProxyServer.java
@@ -6,9 +6,13 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
 import java.util.Random;
+import java.util.Set;
+
 import xyz.hexene.localvpn.Packet.TCPHeader;
 import xyz.hexene.localvpn.TCB.TCBStatus;
 
@@ -37,6 +41,7 @@ public class ProxyServer implements Runnable {
                 int sourcePort = tcpHeader.sourcePort;
                 String ipAndPort = destinationAddress.getHostAddress() + ":" + destinationPort + ":" + sourcePort;
                 packet.swapSourceAndDestination();
+                packet.backingBuffer=null;
                 TCB tcb=TCB.getTCB(ipAndPort);
                 ByteBuffer responseBuffer = ByteBufferPool.acquire();
                 if(tcb==null){
@@ -46,38 +51,45 @@ public class ProxyServer implements Runnable {
                     remoteChannel.connect(new InetSocketAddress(destinationAddress, destinationPort));
                     tcb=new TCB(ipAndPort,random.nextInt(Short.MAX_VALUE),tcpHeader.sequenceNumber, tcpHeader.sequenceNumber, tcpHeader.acknowledgementNumber, remoteChannel, packet);
                     tcb.status=TCBStatus.LISTEN;
+                    TCB.putTCB(ipAndPort,tcb);
                 }
-                tcb.theirAcknowledgementNum=tcpHeader.acknowledgementNumber;
-                tcb.theirSequenceNum=tcpHeader.sequenceNumber;
-                tcb.referencePacket=packet;
-                if(tcpHeader.isSYN()){
-                    packet.updateTCPBuffer(responseBuffer,(byte)(TCPHeader.SYN|TCPHeader.ACK),tcb.mySequenceNum,packet.tcpHeader.sequenceNumber+1,0);
-                    sendDataToClient(responseBuffer);
-                    tcb.status=TCBStatus.SYN_RECEIVED;
-                }else if(tcpHeader.isACK()){
-                    if(tcb.status==TCBStatus.SYN_RECEIVED){
-                        tcb.status=TCBStatus.ESTABLISHED;
-                    }else if(tcb.status==TCBStatus.ESTABLISHED){//连接成功.可以传输数据
-                        System.out.println("client确认收到数据包");
-                        //todo 更新tcb的seq和ack的值,后续发送时更新
-                        tcb.mySequenceNum=tcpHeader.acknowledgementNumber;
-                    }else if(tcb.status==TCBStatus.CLOSE_WAIT){
+                synchronized (tcb){
+                    tcb.theirAcknowledgementNum=tcpHeader.acknowledgementNumber;
+                    tcb.theirSequenceNum=tcpHeader.sequenceNumber;
+                    tcb.referencePacket=packet;
+                    if(tcpHeader.isSYN()){
+                        packet.updateTCPBuffer(responseBuffer,(byte)(TCPHeader.SYN|TCPHeader.ACK),tcb.mySequenceNum,packet.tcpHeader.sequenceNumber+1,0);
+                        sendDataToClient(packet.backingBuffer);
+                        tcb.status=TCBStatus.SYN_RECEIVED;
+                    }else if(tcpHeader.isACK()){
+                        if(tcb.status==TCBStatus.SYN_RECEIVED){
+                            if(tcb.channel.finishConnect()){
+                                tcb.status=TCBStatus.ESTABLISHED;
+                                tcb.selectionKey=tcb.channel.register(tcpSelector, SelectionKey.OP_READ,tcb);
+                            }else {
+                                packet.updateTCPBuffer(responseBuffer,(byte)TCPHeader.RST,0,0,0);
+                                sendDataToClient(packet.backingBuffer);
+                                TCB.closeTCB(tcb);
+                            }
+                        }else if(tcb.status==TCBStatus.ESTABLISHED){//连接成功.可以传输数据
+                            //todo 更新tcb的seq和ack的值,后续发送时更新
+                            tcb.mySequenceNum=tcpHeader.acknowledgementNumber;
+                        }else if(tcb.status==TCBStatus.LAST_ACK){//关闭连接
+                            TCB.closeTCB(tcb);
+                        }
+                    }else if(tcpHeader.isPSH()){
+                        //todo :需要做什么呢
+                    }else if(tcpHeader.isURG()){
+                        //todo :需要怎么做呢
+                    }else if(tcpHeader.isFIN()){
                         packet.updateTCPBuffer(responseBuffer,(byte)(TCPHeader.ACK),0,packet.tcpHeader.sequenceNumber+1,0);
-                        sendDataToClient(responseBuffer);
-                        tcb.status=TCBStatus.LAST_ACK;
+                        sendDataToClient(packet.backingBuffer);
+                        tcb.status=TCBStatus.CLOSE_WAIT;
+                    }else if(tcpHeader.isRST()){
+                        packet.updateTCPBuffer(responseBuffer,(byte)(TCPHeader.ACK),0,packet.tcpHeader.sequenceNumber+1,0);
+                        sendDataToClient(packet.backingBuffer);
+                        tcb.status=TCBStatus.CLOSE_WAIT;
                     }
-                }else if(tcpHeader.isPSH()){
-                    //todo :需要做什么呢
-                }else if(tcpHeader.isURG()){
-                    //todo :需要怎么做呢
-                }else if(tcpHeader.isFIN()){
-                    packet.updateTCPBuffer(responseBuffer,(byte)(TCPHeader.ACK),0,packet.tcpHeader.sequenceNumber+1,0);
-                    sendDataToClient(responseBuffer);
-                    tcb.status=TCBStatus.CLOSE_WAIT;
-                }else if(tcpHeader.isRST()){
-                    packet.updateTCPBuffer(responseBuffer,(byte)(TCPHeader.ACK),0,packet.tcpHeader.sequenceNumber+1,0);
-                    sendDataToClient(responseBuffer);
-                    tcb.status=TCBStatus.CLOSE_WAIT;
                 }
             }else if(packet.isUDP()){
 
@@ -89,6 +101,7 @@ public class ProxyServer implements Runnable {
 
     public void sendDataToClient(ByteBuffer byteBuffer){
         try{
+            byteBuffer.flip();
             clientChannel.write(byteBuffer);
         }catch (Exception e){
             e.printStackTrace();
@@ -100,7 +113,70 @@ public class ProxyServer implements Runnable {
     public void run() {
         while (!Thread.interrupted()){
             try {
-
+                if(tcpSelector.select()<=0){
+                    continue;
+                }
+                Set<SelectionKey> keys = tcpSelector.selectedKeys();
+                Iterator<SelectionKey> keyIterator = keys.iterator();
+                while (keyIterator.hasNext() && !Thread.interrupted()) {
+                    keyIterator.remove();
+                    SelectionKey key = keyIterator.next();
+                    TCB tcb= (TCB) key.attachment();
+                    synchronized (tcb){
+                        if (!key.isValid()) {
+                            continue;
+                        }
+                        if (!key.isReadable()){ //服务端发送数据过来了
+                            continue;
+                        }
+                        ByteBuffer responseBuffer = ByteBufferPool.acquire();
+                        switch (tcb.status){
+                            case ESTABLISHED: //连接建立，可互传数据
+                                try{
+                                    Packet packet=tcb.referencePacket;
+                                    int headerLength=packet.ip4Header.headerChecksum+packet.tcpHeader.headerLength;
+                                    responseBuffer.position(headerLength);
+                                    int length=tcb.channel.read(responseBuffer);
+                                    if(length>0){
+                                        packet.updateTCPBuffer(responseBuffer,(byte) 0,tcb.mySequenceNum,0,responseBuffer.limit()-headerLength);
+                                        sendDataToClient(packet.backingBuffer);
+                                    }else {
+                                        packet.updateTCPBuffer(responseBuffer,(byte) TCPHeader.FIN,tcb.mySequenceNum,0,0);
+                                        sendDataToClient(packet.backingBuffer);
+                                    }
+                                }catch (Exception e){
+                                    e.printStackTrace();
+                                    tcb.referencePacket.updateTCPBuffer(responseBuffer,(byte)TCPHeader.RST,0,0,0);
+                                    sendDataToClient(tcb.referencePacket.backingBuffer);
+                                    TCB.closeTCB(tcb);
+                                }
+                                break;
+                            case CLOSE_WAIT: //服务端在断开之前发送剩余数据
+                                try{
+                                    Packet packet=tcb.referencePacket;
+                                    int headerLength=packet.ip4Header.headerChecksum+packet.tcpHeader.headerLength;
+                                    responseBuffer.position(headerLength);
+                                    int length=tcb.channel.read(responseBuffer);
+                                    if(length>0){
+                                        packet.updateTCPBuffer(responseBuffer,(byte) 0,tcb.mySequenceNum,0,responseBuffer.limit()-headerLength);
+                                        sendDataToClient(packet.backingBuffer);
+                                    }else {
+                                        packet.updateTCPBuffer(responseBuffer,(byte) TCPHeader.FIN,tcb.mySequenceNum,0,0);
+                                        sendDataToClient(packet.backingBuffer);
+                                        tcb.status=TCBStatus.LAST_ACK;
+                                    }
+                                }catch (Exception e){
+                                    e.printStackTrace();
+                                    tcb.referencePacket.updateTCPBuffer(responseBuffer,(byte)TCPHeader.RST,0,0,0);
+                                    sendDataToClient(tcb.referencePacket.backingBuffer);
+                                    TCB.closeTCB(tcb);
+                                }
+                                break;
+                            case LAST_ACK: //服务端完成数据发送，确认关闭
+                                break;
+                        }
+                    }
+                }
             }catch (Exception e){
                 e.printStackTrace();
             }
